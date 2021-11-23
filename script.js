000cc9c1ac853c5944b6fdce77270a4bc00eeca0
josevidmal <92562836+josevidmal@users.noreply.github.com>
2021-11-23 09:18:45 -0600

    Adding Pseudocode as comments in JavaScript file

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+/* Pseudocode:
+1 - User clicks button to initiate password criteria prompts
+2 - User enters the number of characters for the password in prompt window
+3 - User confirms if the password should have lowercase characters
+4 - User confirms if the password should have uppercase characters
+5 - User confirms if the password should have numeric characters
+6 - User confirms if the password should have special characters
+7 - User criteria choices are validated and password is generated with
+random values of the selected character types
+8 - Password is written to the page or in an alert window
+*/
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -12,3 +24,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
